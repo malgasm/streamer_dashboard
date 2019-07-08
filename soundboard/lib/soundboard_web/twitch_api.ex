4c5ca6f25009730d8fa741fcad2d66940ccfe6ec
Kintel Williamson <malgasm@users.noreply.github.com>
2019-07-07 20:45:33 -0700

    twitch API WIP

---
+++ soundboard/lib/soundboard_web/twitch_api.ex
@@ -0,0 +1,14 @@
+defmodule SoundboardWeb.TwitchApi do
+  use Tesla
+
+  plug Tesla.Middleware.BaseUrl, "https://api.twitch.tv/kraken"
+  plug Tesla.Middleware.Headers, [{"Client-ID:", Application.get_env(:soundboard, :twitch_client_id)}]
+  plug Tesla.Middleware.JSON
+
+  #in order to correctly implement this, we'll need to use token exchange, as detailed here https://dev.twitch.tv/docs/authentication/getting-tokens-oauth/#oauth-client-credentials-flow
+
+  def get_user(username) do
+    get("/users?login=#{username}")
+  end
+
+end
--- soundboard/mix.exs
+++ soundboard/mix.exs
@@ -35,13 +35,14 @@ defmodule Soundboard.MixProject do
   defp deps do
     [
       {:exirc, "~> 1.1"},
+      {:gettext, "~> 0.11"},
+      {:jason, "~> 1.0"},
       {:phoenix, "~> 1.4.7"},
       {:phoenix_pubsub, "~> 1.1"},
       {:phoenix_html, "~> 2.11"},
       {:phoenix_live_reload, "~> 1.2", only: :dev},
-      {:gettext, "~> 0.11"},
-      {:jason, "~> 1.0"},
-      {:plug_cowboy, "~> 2.0"}
+      {:plug_cowboy, "~> 2.0"},
+      {:tesla, "~> 1.2.1"}
     ]
   end
 
